9b7f312e8084ae75c29ef3703d817e3f81fb2784
DKC <dake.9ki@gmail.com>
2021-07-13 19:07:17 +0900

    set up additional aws settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -139,6 +139,12 @@ LOGIN_REDIRECT_URL = 'myapp:index'
 # LOGOUT_REDIRECT_URL = 'myapp:logout'
 
 # S3のための記述
+AWS_STORAGE_BUCKET_NAME = 'udemy-django-4180'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400', # 1日はそのキャッシュを使う
+}
+
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # DEFAULT_FILE_STORAGE = 'myproject.backends.MediaStorage'
