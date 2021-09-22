20f462ab543ceece69f3dcfaf3841d53b5763ed2
DKC <dake.9ki@gmail.com>
2021-09-22 18:30:57 +0900

    fix table name of siteconfig

--- sitemanage/models.py
+++ sitemanage/models.py
@@ -6,7 +6,7 @@ from django.contrib.sites.models import Site
 class SiteConfig(models.Model):
     site = models.OneToOneField(Site, verbose_name='Site', on_delete=models.PROTECT)
     meta_title = models.CharField('meta_title', max_length=100)
-    mata_description = models.CharField('meta_description', max_length=300)
+    meta_description = models.CharField('meta_description', max_length=300)
     meta_keyword = models.CharField('SEOキーワード', max_length= 300)
     author = models.CharField('管理者', max_length=30)
     top_title = models.CharField('TOPページタイトル', max_length=100)
